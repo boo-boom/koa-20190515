f46edfdd2d870784ca4aea5908cd655ee1771dd6
boom <510126766@qq.com>
2020-03-13 11:26:46 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -12,8 +12,8 @@ module.exports = {
         expiresIn: 60 * 60,             // jwk过期时间1小时
     },
     wx: {
-        appId: 'wx48b9b67bb96f2ddc',
-        appSecret: '2bfb91b8b146f4eda38b6915389ffa23',
+        appId: '',
+        appSecret: '',
         loginUrl: 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'
     }
 }
